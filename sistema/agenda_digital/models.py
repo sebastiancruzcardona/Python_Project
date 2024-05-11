ea8d8d0a1b27c689e17828217f3cf6489ef44cb0
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2024-05-11 18:24:28 -0500

    feat(models.py): Create usuario class and migrate it to database

--- sistema/agenda_digital/models.py
+++ sistema/agenda_digital/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class usuario(models.Model):
+   id = models.AutoField(primary_key=True)
+   nombre = models.CharField(max_length=60)
+   email = models.CharField(max_length=60)
+   telefono = models.CharField(max_length=10)
+   password = models.CharField(max_length=50)
