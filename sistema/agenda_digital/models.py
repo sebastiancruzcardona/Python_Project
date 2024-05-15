6b42b3e943192f5b8e085988a43be2b8692cbb61
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2024-05-14 19:49:37 -0500

    feat(models.py): Add verbose_name attribute to attributes of usuario class

--- sistema/agenda_digital/models.py
+++ sistema/agenda_digital/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class usuario(models.Model):
    id = models.AutoField(primary_key=True)
-   nombre = models.CharField(max_length=60)
-   email = models.CharField(max_length=60)
-   telefono = models.CharField(max_length=10)
-   password = models.CharField(max_length=50)
+   nombre = models.CharField(max_length=60, verbose_name="Nombre")
+   email = models.CharField(max_length=60, verbose_name="Email")
+   telefono = models.CharField(max_length=10, verbose_name="Teléfono")
+   password = models.CharField(max_length=50, verbose_name="Contraseña")
