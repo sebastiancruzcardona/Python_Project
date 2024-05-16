7aaef5db37cf63384e306d74a0d6f06c9ffc4b69
lugoDv <falugopersonal@gmail.com>
2024-05-15 21:00:07 -0500

    feat (import) models in admin

--- sistema/agenda_digital/admin.py
+++ sistema/agenda_digital/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import usuario
+from .models import contacto
+
 # Register your models here.
+
 admin.site.register(usuario)
+admin.site.register(contacto)
