37508d8edf9ef97e522f0584a259f3b5a77616a8
sebastiancruzcardona <sebastiancruzcardona90@gmail.com>
2024-05-16 18:44:46 -0500

    feat(models.py): Change email field in class usuario to unique for database

--- sistema/agenda_digital/models.py
+++ sistema/agenda_digital/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class usuario(models.Model):
    id = models.AutoField(primary_key=True)
    nombre = models.CharField(max_length=60, verbose_name="Nombre")
-   email = models.CharField(max_length=60, verbose_name="Email")
+   email = models.CharField(max_length=60, unique=True, verbose_name="Email")
    telefono = models.CharField(max_length=10, verbose_name="Teléfono")
    password = models.CharField(max_length=50, verbose_name="Contraseña")
 
